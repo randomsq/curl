bc8fadfdef2151503af7918bc6a5780d515bd937
Rohit Kashyap <rohitkashayap802@gmail.com>
2020-01-17 14:07:51 +0530

    an example of get share count through an api
    here we are using two methods to get file contents

---
+++ getFileContents.php
@@ -0,0 +1,20 @@
+<?php
+$url = "http://count-server.sharethis.com/v2.0/get_counts?wd=true&url=https://lawstreet.co/executive/pm-modi-denies-the-existence-of-detention-centers/";
+//  Initiate curl
+$ch = curl_init();
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+$result=curl_exec($ch);
+// Closing
+curl_close($ch);
+
+// Will dump a beauty json :3
+var_dump(json_decode($result, true));
+
+$result = file_get_contents($url);
+// Will dump a beauty json :3
+var_dump(json_decode($result, true));
+?>
